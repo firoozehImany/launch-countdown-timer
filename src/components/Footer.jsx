96df56c48ed242ddd4cea9add0fbb650b843b941
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2026-03-26 15:56:09 +0100

    test-001

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Mountains from "./Mountains";
 
 export default function Footer() {
   return (
-    <footer className="relative w-full overflow-hidden opacity-25">
+    <footer className="relative w-full overflow-hidden">
       <Mountains />
       <div className="absolute inset-x-0 bottom-10 md:bottom-16 z-10 flex justify-center">
         <SocialLinks />
